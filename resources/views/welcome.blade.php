8e99c81050ce409bb00f33df41b8d712285f511d
umasharma316 <umasharma316@gmail.com>
2021-12-21 13:26:30 +0530

    added changes abour abrevation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('eYSIP')])
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('eYIP')])
 
 @section('content')
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
-          <h1 class="text-white text-center">{{ __('Welcome to e-Yantra Summer Internship Program.') }}</h1>
+          <h1 class="text-white text-center">{{ __('Welcome to e-Yantra Internship Program.') }}</h1>
       </div>
   </div>
 </div>
