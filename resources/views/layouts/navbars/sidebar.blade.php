11dfaabe88028e65382dafd83e1e677e0ff92e19
Aayushi gautam <52914397+aayushi363@users.noreply.github.com>
2022-05-07 11:26:59 +0530

    Timeslot booking enabled

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -60,12 +60,12 @@
       </li> -->
   @if(Auth::user()->role == 1)
     @if( Auth::user()->profilesubmitted == 1)
-    <!-- <li class="nav-item{{ $activePage == 'timeslotbooking' ? ' active' : '' }}">
+    <li class="nav-item{{ $activePage == 'timeslotbooking' ? ' active' : '' }}">
           <a class="nav-link" href="{{ route('timeslotbooking') }}">
             <i class="material-icons">library_books</i>
               <p>{{ __('Interview Slot') }}</p>
           </a>
-        </li> -->
+        </li>
         <li class="nav-item{{ $activePage == 'preference' ? ' active' : '' }}">
           <a class="nav-link" href="{{ route('projectpreference') }}">
             <i class="material-icons">content_paste</i>
