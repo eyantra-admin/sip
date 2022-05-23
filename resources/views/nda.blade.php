047c4aa87b46fe9ce1fd69e1e17a3d25343c7fc9
Aayushi gautam <52914397+aayushi363@users.noreply.github.com>
2022-05-23 17:58:33 +0530

    updated nda

--- resources/views/nda.blade.php
+++ resources/views/nda.blade.php
@@ -27,12 +27,12 @@
 
       <div class="card">
         <div class="card-header card-header-primary">
-            <h4 class="card-title">{{ __('eYIP - Non Disclosure Agreement ') }}</h4>
-            (to be signed by intern, participating in the e-Yantra Internship Program [eYIP])
+            <h4 class="card-title">{{ __('eYSIP - Non Disclosure Agreement ') }}</h4>
+            (to be signed by intern, participating in the e-Yantra Summer Internship Program [eYSIP])
         </div>
         <div class="card-body">
           <h4><b>
-            I, <b>{{Auth::user()->name}}</b> participating in the e-Yantra Internship Program (eYIP) conducted by the e-Yantra project of IIT Bombay, <br><br>
+            I, <b>{{Auth::user()->name}}</b> participating in the e-Yantra Summer Internship Program (eYSIP) conducted by the e-Yantra project of IIT Bombay, <br><br>
 
             <ol>
               <li>
@@ -41,13 +41,13 @@
              <li>Understand that if required to disclose or display the working of the project that was submitted for e-Yantra in any public forum, I may do so ONLY after PRIOR approval of e-Yantra, IIT Bombay.</li>
              <li>Understand that any and all data, information, related to the project disclosed or furnished by or on behalf of e-Yantra is the sole and absolute property of e-Yantra, IIT Bombay.</li>
              <li>Understand that, NO material, data or information is to be copied/replicated/reproduced or stored without written permission of e-Yantra, IIT Bombay and the contents thereof must not be imparted/disseminated or disclosed for any unauthorized purpose and/or usage or any other reason whatsoever on any version control platforms like GitHub, Bitbucket etc.</li>
-             <li>Understand that Indian Institute of Technology Bombay and e-Yantra shall own all the code and content which is created during eYIP or as a result thereof.</li>
+             <li>Understand that Indian Institute of Technology Bombay and e-Yantra shall own all the code and content which is created during eYSIP or as a result thereof.</li>
              <li>Understand that logos of e-Yantra and IIT Bombay shall not be used/utilized for/in any promotional or other event/activity by the student. Any use of the e-Yantra name/logo in any manner whatsoever needs prior written approval from the e-Yantra project, IIT Bombay.</li>
-             <li>Understand that I am not allowed to claim employment with e-Yantra or IIT Bombay on online social media platforms such as Facebook, LinkedIn etc without prior approval from e-Yantra. I am, however, allowed to claim association with e-Yantra as an e-YIP Intern between the time frame of January 2022 - March 2022.</li>
-             <li>Understand that eYIP is a full-time commitment and I need to put in at least 40 hours of work per week. I also understand that I am not allowed to participate in any other internship/job while being an active eYIP intern.</li>
+             <li>Understand that I am not allowed to claim employment with e-Yantra or IIT Bombay on online social media platforms such as Facebook, LinkedIn etc without prior approval from e-Yantra. I am, however, allowed to claim association with e-Yantra as an e-YIP Intern between the time frame of June 2022 - July 2022.</li>
+             <li>Understand that eYSIP is a full-time commitment and I need to put in at least 40 hours of work per week. I also understand that I am not allowed to participate in any other internship/job while being an active eYSIP intern.</li>
              <li>Understand that I am not eligible for a ‘Letter of Recommendation’ from any of the e-Yantra team members</li>
              <li>Understand that copyright of work products that arise out of my/our work for the project will be held by and shall vest in e-Yantra, IIT Bombay.</li>
-             <li>Any publication arising from the work under eYIP shall be duly acknowledged in a form and manner to the satisfaction of e-Yantra team, IIT Bombay and only submitted for publication after taking due written permission.</li>
+             <li>Any publication arising from the work under eYSIP shall be duly acknowledged in a form and manner to the satisfaction of e-Yantra team, IIT Bombay and only submitted for publication after taking due written permission.</li>
            </ol>
           </b></h4>
           <div style="background-color: #f1cbf7; border-radius: 5px; margin: 50px">
