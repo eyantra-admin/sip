70c70b45a6f7310e68adb0e5343af979cf09a325
deepaavudiappan <deepaavudiappan@gmail.com>
2022-09-19 12:18:56 +0530

    changes in certificate

--- resources/views/template/student_merit.blade.php
+++ resources/views/template/student_merit.blade.php
@@ -2,10 +2,7 @@
 @section('content')
 <div>
     <p>
-    	This certificate is awarded to <b>{{$student_details->name}}</b>, a student of B.Tech from <b>{{$student_details->college}}</b>  who has successfully completed eight-weeks (from 3rd January 2022 to 25th of February 2022) online <b>Product Design Internship programme</b> at <b>e-Yantra, IIT Bombay</b>.<br><br>
-        He/She is a member of the team having the following team member:<br>
-        <b>{{$student_details->team_member}}</b><br><br>
-        This team implemented and completed the task with the topic name: <b>{{$project_name->projectname}}</b><br>
+        This is to certify that <b>{{$student_details->name}}</b>, student from <b>{{$student_details->college}}</b> has undertaken Internship at e-Yantra, IIT Bombay working on a project entitled: <b>{{$project_name->projectname}}</b> during the period from <b>{!! $student_details->proj_duration !!}</b> and has successfully completed the same.
     </p>
 </div>
 @endsection
