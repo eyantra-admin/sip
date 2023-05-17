7a8c3ab71c7f160227d1eb02e311b31b909d97ad
sachinuthale <sachinuthale@gmail.com>
2023-05-17 17:13:47 +0530

    payment guzzle verify false

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -307,7 +307,8 @@ class PaymentController extends Controller
                     //create another client without token in header
                     $client= new Client([
                         'Content-Type' => 'application/json',
-                        'base_uri' => $server->base_uri
+                        'base_uri' => $server->base_uri,
+                        'verify' => false,
                     ]);
 
                     //make a request to get new token
