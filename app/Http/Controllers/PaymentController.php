3e5d463a9ef8bd0bf5e2a2fd40777fecc5f2e5ad
sachinuthale <sachinuthale@gmail.com>
2023-05-17 20:03:15 +0530

    payment backup page

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -375,8 +375,9 @@ class PaymentController extends Controller
                         'purpose' => 'eYSIP2023',
                         'currency' => 'INR',
                     ],
-                    'cert' => storage_path().'/payment_ssl/epay_eyantra_cse_iitb_ac_in.crt',
-                    'ssl_key' => storage_path().'/payment_ssl/epay_eyantra_cse_iitb_ac_in.key'
+                    'verify' => false,
+                    //'cert' => storage_path().'/payment_ssl/epay_eyantra_cse_iitb_ac_in.crt',
+                    //'ssl_key' => storage_path().'/payment_ssl/epay_eyantra_cse_iitb_ac_in.key'
                 ])->getBody();
                 Log::info($response);
             
