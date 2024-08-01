75d284d0aa14f6d9a3d9d67a1c4e226bd0afbbe2
deepaavudiappan <deepaavudiappan@gmail.com>
2024-08-01 18:54:24 +0530

    changes for external projects

--- resources/views/template/student_merit.blade.php
+++ resources/views/template/student_merit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div>
     <p>
-    	@if({{$student_details->external_proj == 0}} )
+    	@if({{$student_details->external_proj}} == 0)
         This is to certify that <b>{{$student_details->name}}</b>, student from <b>{{$student_details->college}}</b> has undertaken Internship at <nobr>e-Yantra</nobr>, IIT Bombay working on a project entitled: <b>{{$project_name->projectname}}</b> during the period from <b>{!! $student_details->proj_duration !!}</b> and has successfully completed the same.
         @else
         This is to certify that <b>{{$student_details->name}}</b>, student from <b>{{$student_details->college}}</b> has undertaken Internship at <nobr>e-Yantra</nobr>, IIT Bombay working on a project entitled: <b>{{$project_name->projectname}}</b> during the period from <b>{!! $student_details->proj_duration !!}</b>. This project was successfully completed in collaboration with {{$student_details->external_dept}}.
